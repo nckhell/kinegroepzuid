5aeb85811031d1ef2096e0c69a12d8221b8cf38d
Nick Hellemans <NickHellemans93@gmail.com>
2022-07-20 22:02:32 +0200

    chore: install redirects from previous website

--- next.config.js
+++ next.config.js
@@ -5,6 +5,35 @@ const nextConfig = {
     webpack5: true, // by default, if you customize webpack config, they switch back to version 4.
     // Looks like backward compatibility approach.
   },
+  async redirects() {
+    return [
+      {
+        source: '/team',
+        destination: '/#team',
+        permanent: true,
+      },
+      {
+        source: '/info',
+        destination: '/#info',
+        permanent: true,
+      },
+      {
+        source: '/specialisaties',
+        destination: '/#specialisaties',
+        permanent: true,
+      },
+      {
+        source: '/ligging',
+        destination: '/#ligging',
+        permanent: true,
+      },
+      {
+        source: '/afspraak',
+        destination: '/#afspraak',
+        permanent: true,
+      },
+    ]
+  },
   webpack(config) {
     config.resolve.fallback = {
       ...config.resolve.fallback, // if you miss it, all the other options in fallback, specified
